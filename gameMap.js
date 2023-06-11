0afa07ceaf51ca5a24ad9f2519933ce90ba20f61
BuildTools <unconfigured@null.spigotmc.org>
2023-06-11 14:37:55 -0400

    d

--- gameMap.js
+++ gameMap.js
@@ -14,7 +14,20 @@ class gameMap {
     draw(ctx) {
         console.log(this.x)
         console.log(this.y)
+        console.log(this.length)
         console.log(this.image.naturalWidth)
-        ctx.drawImage(this.image, 0, 54, this.image.naturalWidth, 13, this.x, 120, this.length, this.height)
+
+        let j = 0        
+        
+        console.log(this.length/(this.image.naturalWidth - (this.image.naturalWidth - 1201)) - 1)
+
+        for (let i = 0; i<this.length/(this.image.naturalWidth - (this.image.naturalWidth - 1201)) - 1; i++) {
+            j++
+            console.log(i)
+            ctx.drawImage(this.image, 2, 54, this.image.naturalWidth - (this.image.naturalWidth - 1201), 13, (this.image.naturalWidth - (this.image.naturalWidth - 1201)) * i, this.y, (this.image.naturalWidth - (this.image.naturalWidth - 1201)), this.height) // the 2nd and 3rd values are where the road starts on the image, and 1201 is where the road ends
+        }
+
+        ctx.drawImage(this.image, 2, 54, this.length%(this.image.naturalWidth - (this.image.naturalWidth - 1201)), 13, (this.image.naturalWidth - (this.image.naturalWidth - 1201)) * j, this.y, this.length%(this.image.naturalWidth - (this.image.naturalWidth - 1201)), this.height) // the 2nd and 3rd values are where the road starts on the image, and 1201 is where the road ends
+
     }
 }
--- index.js
+++ index.js
@@ -6,15 +6,104 @@ canvasCTX.imageSmoothingEnabled = false
 const spriteSheetSrc = "images/spritemain.png" 
 let toggled = false
 
+function initCanvas() {
+    console.log("text")
+    canvasCTX.font = "18px Fantasy"
+    canvasCTX.textAlign = "center"
+    canvasCTX.fillText("CLICK TO START", canvas.width/2, canvas.height/2)
+}
+
+function fadeOut() {
+    const duration = 500
+    let start
+    let called = false
+
+    function loop(time) {
+        if (!start) {
+            start = time
+            requestAnimationFrame(loop)
+            return
+        }
+
+        const alpha = Math.max(1 - (time - start)/duration, 0)
+
+        canvasCTX.clearRect(0, 0, canvas.width, canvas.height)
+
+        canvasCTX.fillStyle = `rgba(0, 0, 0, ${alpha})`
+        canvasCTX.font = "18px Fantasy"
+        canvasCTX.textAlign = "center"
+        canvasCTX.fillText("CLICK TO START", canvas.width/2, canvas.height/2)
+
+        if (alpha <= 0.3 && !called) {
+            console.log("CALLED FUNC")
+            called = true
+            resizeCanvas()
+        }
+
+        if (alpha == 0) {
+            canvasCTX.clearRect(0, 0, canvas.width, canvas.height)
+        } else {
+            requestAnimationFrame(loop)
+        }
+    }
+
+    requestAnimationFrame(loop)
+}
+
+function resizeCanvas() {
+    console.log("canvas resize called!")
+
+    toggled = "in progress"
+    let start
+
+    const duration = 400
+    const x = window.innerWidth
+    const y = window.innerHeight * 0.15
+
+    const initAnim = time => {
+        if (!start) {
+            start = time
+            window.requestAnimationFrame(initAnim)
+            return
+        }
+
+        const progress = Math.pow((time - start)/duration, 2)
+
+        console.log("progress - " + progress)
+
+        if (progress < 1) {
+            canvas.style.width = lerp(window.innerWidth * 0.10, x, progress) + "px"
+            canvas.style.height = lerp(window.innerHeight * 0.05, y, progress) + "px"
+            window.requestAnimationFrame(initAnim)
+        } else {
+            canvas.style.width = x + "px"
+            canvas.style.height = y + "px"
+            canvas.width = x
+            canvas.height = y
+            toggled = true
+            init()
+        }
+    }
+
+    requestAnimationFrame(initAnim)
+}
+
 function resize() {
     if (toggled === false) {
         canvas.style.height = window.innerHeight * 0.05 + "px"
         canvas.style.width = window.innerWidth * 0.10 + "px"
+
+        canvas.height = window.innerHeight * 0.05
+        canvas.width = window.innerWidth * 0.10
+
         console.log("hasn't clicked")
     } else if (toggled === true) {
         console.log("hi")
         canvas.style.height = window.innerHeight * 0.15 + "px"
         canvas.style.width = window.innerWidth + "px"
+
+        canvas.height = window.innerHeight * 0.15
+        canvas.width = window.innerWidth
     }
 }
 
@@ -23,50 +112,20 @@ function init() {
     img.src = "images/spritemain.png"
 
     img.onload = () => {
-        const map = new gameMap(canvas.offsetWidth, canvas.offsetHeight/20, img, 0, canvas.offsetHeight - canvas.offsetHeight/20)
+        const map = new gameMap(canvas.offsetWidth, canvas.offsetHeight/20, img, 0, canvas.offsetHeight - canvas.offsetHeight/15)
         map.draw(canvasCTX)
     }
 }
 
 
 resize()
+initCanvas()
+
 window.addEventListener("resize", resize)
 
 canvas.addEventListener("click", () => {
     if (!toggled) {
-        console.log("here!")
-
-        toggled = "in progress"
-        let start
-
-        const duration = 500
-        const x = window.innerWidth
-        const y = window.innerHeight * 0.15
-
-        const initAnim = time => {
-            if (!start) {
-                start = time
-                window.requestAnimationFrame(initAnim)
-                return
-            }
-
-            const progress = (time - start)/duration
-
-            console.log("progress - " + progress)
-
-            if (progress < 1) {
-                canvas.style.width = lerp(window.innerWidth * 0.10, x, progress) + "px"
-                canvas.style.height = lerp(window.innerHeight * 0.05, y, progress) + "px"
-                window.requestAnimationFrame(initAnim)
-            } else {
-                canvas.style.width = x + "px"
-                canvas.style.height = y + "px"
-                toggled = true
-                init()
-            }
-        }
-
-        requestAnimationFrame(initAnim)
+        fadeOut()
     }
 })
 
