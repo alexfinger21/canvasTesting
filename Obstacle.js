6dc28bf46bc22d85c2fc357c1b00df7260497125
alexfinger21 <cdx9933@gmail.com>
2023-06-16 23:40:29 -0400

    added comment

--- Obstacle.js
+++ Obstacle.js
@@ -1,6 +1,6 @@
 class Obstacle {
     constructor(obstacleChance = 0.01) {
-        this.obstacleChance = 0.01
+        this.obstacleChance = 0.01//add coordinates for the spritesheet
     }
 
     draw(ctx) {
