04b89204c4d158f587c5119ae9ee6ad766701b88
BuildTools <unconfigured@null.spigotmc.org>
2023-06-20 13:19:11 -0400

    d

--- Obstacle.js
+++ Obstacle.js
@@ -1,5 +1,5 @@
 class Obstacle {
-    constructor(obstacleChance = 0.01, yPos) {
+    constructor(obstacleChance = 0.01, yPos, img) {
         this.obstacleChance = obstacleChance 
 
         this.lastSpawned = 0
@@ -9,14 +9,17 @@ class Obstacle {
 
         this.minDist = 50
 
-        this.cacti = [[332, 34], [357, 23], [383, 23], [407, 23], [431, 50]]
+        this.cacti = [[332, 24], [357, 23], [383, 23], [407, 23], [431, 50]]
 
+        this.image = img
     }
 
     draw(ctx) {
         ctx.save()
 
-        //Math.random()
+        const cactus = Math.floor(Math.random() * this.cacti.length)
+
+        ctx.drawImage(this.image, this.cacti[cactus][0], 1, this.cacti[cactus][1], this.height, Math.random() * 9000 + 100, this.y, this.cacti[cactus][1], this.height) // the 2nd and 3rd values are where the road starts on the image, and 1200 is where the road ends
 
         ctx.restore()
     }
